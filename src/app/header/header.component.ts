4c2a2f56e61d8dd9551dbec5a9fd4488de5dedad
AlexandraTsvetkova <alexandra.cvetkova@neuralegion.com>
2024-12-22 13:31:55 +0100

    fix: header typo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,7 +12,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  title = 'Pet Foood Calculations';
+  title = 'Pet Food Calculations';
   creditsShown = false;
 
   onCreditsClick() {
